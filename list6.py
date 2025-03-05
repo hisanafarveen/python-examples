dd0b5fea0431beee2119c674e4dce5a9fcb792f9
hisanafarveen <hisanafarween@gmail.com>
2025-03-05 09:31:30 +0530

    Add files via upload

---
+++ list6.py
@@ -0,0 +1,7 @@
+data=input("enter a value or 0 for quit")
+while data!='0':
+    if data=='8':
+        data=input("enter a value 0 for quit")
+        continue
+    data=input("enter a value 0 for quit") 
+    
